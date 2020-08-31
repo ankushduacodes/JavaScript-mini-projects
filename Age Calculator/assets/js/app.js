f7fd8c01b5ec6f5fdd3b47fc4e2948bea569a9dd
ankushduacodes <61025943+ankushduacodes@users.noreply.github.com>
2020-08-31 21:37:42 +0530

    fixed date validity bug it alerts if date input box is empty added todo list

--- Age Calculator/assets/js/app.js
+++ Age Calculator/assets/js/app.js
@@ -2,13 +2,30 @@ let today = new Date().toISOString().slice(0, 10);
 
 document.getElementById("mydate").max = today;
 
+calculateBtn = document.getElementById("calculate-btn")
+
 result = document.getElementById("result")
 
-function showResult(resultOutput) {
-  result.textContent = resultOutput;
+function checkValidInput() {
+  if (document.getElementById('mydate').value === "") {
+    alert("Missing date")
+    return false;
+  }
+  return true;
+}
+
+function showResult(age) {
+  result.textContent = ` You are ${age} years old`;
 }
 
 function calculate() {
+  if (!checkValidInput()) {
+    return;
+  }
   dateOfBirth = document.getElementById("mydate").value.split("-");
-  todayDate = today
+  todayDate = today.split("-")
+  age = todayDate[0] - dateOfBirth[0]
+  showResult(age)
 }
+
+calculateBtn.addEventListener("click", calculate)
