fca10fb7cd6ffaa8bfec47f2feab183f507c88fc
ankushduacodes <61025943+ankushduacodes@users.noreply.github.com>
2020-08-31 21:34:03 +0530

    added calculate function to calculate age

--- Age Calculator/assets/js/app.js
+++ Age Calculator/assets/js/app.js
@@ -1,4 +1,14 @@
-let today = new Date().toISOString().slice(0, 10)
+let today = new Date().toISOString().slice(0, 10);
 
-let maxDate = document.getElementById("mydate").max = today
+document.getElementById("mydate").max = today;
 
+result = document.getElementById("result")
+
+function showResult(resultOutput) {
+  result.textContent = resultOutput;
+}
+
+function calculate() {
+  dateOfBirth = document.getElementById("mydate").value.split("-");
+  todayDate = today
+}
